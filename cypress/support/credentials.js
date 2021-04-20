94da9f9ab474db8ab1f14d7ec2bc9dcc20705d03
Lucas Fraga <lucass.fragaa@gmail.com>
2021-04-20 19:34:20 -0300

    subindo novos arquivos

---
+++ cypress/support/credentials.js
@@ -0,0 +1,26 @@
+/// <reference types="cypress" />
+
+const httpStatus = require('http-status-codes')
+
+const credentials = {
+  HEADERS: {
+    'Content-type': 'application/json'
+  }
+}
+
+Cypress.Commands.add('getToken', () => {
+  cy.log('Get token - Bearer')
+  cy.doLogin(Cypress.env('email'), Cypress.env('password'))
+    .then(response => {
+      localStorage.setItem('token', response.body.authorization)
+      expect(response.status).to.eq(httpStatus.StatusCodes.OK)
+      expect(localStorage.getItem('token')).not.null // eslint-disable-line
+      cy.log(localStorage.getItem('token'))
+    })
+})
+
+Cypress.Commands.add('clearSession', () => {
+  localStorage.removeItem('token')
+})
+
+export default credentials
