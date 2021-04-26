42993844316c7a7b064340b6fd543da20cd301f6
Lucas Fraga <lucass.fragaa@gmail.com>
2021-04-26 15:01:51 -0300

    criando testes verbo GET para produtos

---
+++ cypress/integration/tests/produtos/get.spec.js
@@ -0,0 +1,65 @@
+/// <reference types="cypress" />
+
+import dataProducts from '../../../fixtures/products.json'
+const httpStatus = require('http-status-codes')
+
+describe('[INTEGRATION] :: Listar Produtos', () => {
+  let qtd
+
+  it('/GET - Listar todos os produtos cadastrados', () => {
+    cy.consultProduct()
+      .then((response) => {
+        qtd = response.body.quantidade
+        expect(response.status).to.eq(httpStatus.StatusCodes.OK)
+        expect(response.body.quantidade).to.eq(qtd)
+      })
+  })
+
+  it('/GET - Listar produtos cadastrados pelo _id', () => {
+    cy.consultProduct(dataProducts.produtos[0]._id)
+      .then((response) => {
+        expect(response.status).to.eq(httpStatus.StatusCodes.OK)
+        expect(response.body.produtos[0]._id).to.deep.eq(dataProducts.produtos[0]._id)
+      })
+  })
+
+  it('/GET - Listar produtos cadastrados pelo nome', () => {
+    cy.consultProduct({}, dataProducts.produtos[0].nome)
+      .then((response) => {
+        expect(response.status).to.eq(httpStatus.StatusCodes.OK)
+        expect(response.body.produtos[0].nome).to.deep.eq(dataProducts.produtos[0].nome)
+      })
+  })
+
+  it('/GET - Listar produtos cadastrados pela descrição', () => {
+    cy.consultProduct({}, {}, dataProducts.produtos[0].descricao)
+      .then((response) => {
+        expect(response.status).to.eq(httpStatus.StatusCodes.OK)
+        expect(response.body.produtos[0].descricao).to.deep.eq(dataProducts.produtos[0].descricao)
+      })
+  })
+
+  it('/GET - Listar produtos que não estão cadastrados pelo _id', () => {
+    cy.consultProduct('ID_NAO_EXISTE', {}, {})
+      .then((response) => {
+        expect(response.status).to.eq(httpStatus.StatusCodes.OK)
+        expect(response.body.quantidade).to.deep.eq(0)
+      })
+  })
+
+  it('/GET - Listar produtos que não estão cadastrados pelo nome', () => {
+    cy.consultProduct({}, 'Produto que não existe')
+      .then((response) => {
+        expect(response.status).to.eq(httpStatus.StatusCodes.OK)
+        expect(response.body.quantidade).to.deep.eq(0)
+      })
+  })
+
+  it('/GET - Listar produtos que não estão cadastrados pela descrição', () => {
+    cy.consultProduct({}, {}, 'Descrição que não existe')
+      .then((response) => {
+        expect(response.status).to.eq(httpStatus.StatusCodes.OK)
+        expect(response.body.quantidade).to.deep.eq(0)
+      })
+  })
+})
