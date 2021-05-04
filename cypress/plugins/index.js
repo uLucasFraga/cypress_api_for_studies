251d9f660bd44a734a6437e34bb39bcbbba3f0cc
Lucas Fraga <lucass.fragaa@gmail.com>
2021-05-03 22:33:38 -0300

    lint fix

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,8 +1,8 @@
 /// <reference types="cypress" />
 
-const allureWriter = require('@shelex/cypress-allure-plugin/writer');
+const allureWriter = require('@shelex/cypress-allure-plugin/writer')
 
 module.exports = (on, config) => {
-    allureWriter(on, config);
-    return config;
+  allureWriter(on, config)
+  return config
 }
